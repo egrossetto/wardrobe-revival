977439c691791bb3ef82200e3670a9dc3f67b47e
egrossetto <eugerosso22@hotmail.com>
2023-06-18 22:18:15 -0300

    CSS changes

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,7 +3,11 @@ import CartList from "@/components/Cart/CartList";
 export default function Home() {
   return (
     <main>
-      <h1>Your Wardrobe Revival cart.</h1>
+      <div className="flex align-center justify-center mt-2">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          Your Wardrobe Revival cart.
+        </h1>
+      </div>
       <CartList />
     </main>
   );
