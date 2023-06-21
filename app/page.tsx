a221e69f6bd3780e4afb1e94b6f0ddc65585ac52
egrossetto <eugerosso22@hotmail.com>
2023-06-21 19:28:09 -0300

    Fix width for slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
-import ProductList from "@/components/Product/ProductList";
+import ProductList from '@/components/Product/ProductList';
 
-import { supabase } from "@/lib/supabaseClient";
-import Loading from "./loading";
-import { Suspense } from "react";
+import { supabase } from '@/lib/supabaseClient';
 
 export default async function Home() {
-  const { data } = await supabase.from("products").select();
+	const { data } = await supabase.from('products').select();
 
-  return (
-    <main>
-      <Suspense fallback={<Loading />}>
-        <ProductList serverProducts={data} />
-      </Suspense>
-    </main>
-  );
+	return (
+		<main>
+			<ProductList serverProducts={data} />
+		</main>
+	);
 }
