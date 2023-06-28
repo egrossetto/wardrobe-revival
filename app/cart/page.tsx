053bbaad7a5e828d7ef118133241b04579f2cb7b
egrossetto <eugerosso22@hotmail.com>
2023-06-28 20:28:28 -0300

    add style fixes and remove error of encoding

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,9 +1,9 @@
-import CartList from "@/components/Cart/CartList";
+import CartList from '@/components/Cart/CartList';
 
 export default function Home() {
-  return (
-    <main>
-      <CartList />
-    </main>
-  );
+	return (
+		<main>
+			<CartList />
+		</main>
+	);
 }
